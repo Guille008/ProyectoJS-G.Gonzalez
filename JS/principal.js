c02cb0aa47735b01e6eabace8d743d294c8c79c8
Guille008 <guille15243342@gmail.com>
2025-05-20 16:48:14 -0300

    Lista Preentrega N°1

--- JS/principal.js
+++ JS/principal.js
@@ -1,7 +1,56 @@
-let  Nombre
-let  Apellido
-let  DNI
+let Nombre;
+let DNI;
+let Seleccion;
+let total = 0;
+let Carrito = [];
 
-Nombre = "Guille";
-console.log(Nombre);
-console.log(Nombre +" "+ Nombre);
+Nombre = prompt("Bienvenido a Calzados sport!\nPor favor ingrese su nombre completo");
+while (Nombre === null || !isNaN(Nombre)) {
+    Nombre = prompt("Entrada invalida. Por favor ingrese su nombre");
+}
+DNI =prompt("Gracias por visitar nuestro sitio "+Nombre+"\nPor favor ingrese su nuemero de DNI");
+    if (DNI !== null && !isNaN(DNI)){
+        console.log("Gracias por su visita "+Nombre);
+        
+    } else {
+        console.log("necesita ingresar un numero")
+    }
+
+const productosArr = [];
+
+productosArr.push({ Producto: "Zapatilla 1", Precio: 2000 });
+productosArr.push({ Producto: "Zapatilla 2", Precio: 3000 });
+productosArr.push({ Producto: "Zapatilla 3", Precio: 3500 });
+
+let mensaje = "Seleccione la opcion deseada \n";
+for (let i = 0; i < productosArr.length; i++) {
+    mensaje += (i + 1) + " - " + productosArr[i].Producto + " ($" + productosArr[i].Precio + ")\n";
+}
+
+mensaje += "Escriba 'salir' para finalizar.";
+
+do {
+    opcion = prompt(mensaje);
+    if (opcion === "salir") {
+        break;
+    }
+
+    let index = parseInt(opcion) - 1;
+
+    if (!isNaN(index) && index >= 0 && index < productosArr.length) {
+        total += productosArr[index].Precio;
+        Carrito.push(productosArr[index].Producto);
+        alert("Producto agregado " + productosArr[index].Producto);
+    } else {
+        alert("Opcion Invalida. Por favor vuelva a ingresar.");
+    }
+} while(true);
+console.log("Resumen de su compra \n" +Nombre+ " DNI: "+DNI);
+console.log("Carrito Final " + Carrito.join(", "));
+console.log("Total a pagar: $" +total);
+
+
+
+
+
+
